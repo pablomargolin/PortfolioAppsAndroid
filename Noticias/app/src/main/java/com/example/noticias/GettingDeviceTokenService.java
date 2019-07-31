b93dda395511f0d019703109046f08cef3b5f841
Pablo <pablomargolin@gmail.com>
2019-07-31 17:16:56 -0300

    push  notification

---
+++ Noticias/app/src/main/java/com/example/noticias/GettingDeviceTokenService.java
@@ -0,0 +1,16 @@
+package com.example.noticias;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class GettingDeviceTokenService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String DeviceToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("DEVICE TOKEN: ", DeviceToken);
+    }
+}
